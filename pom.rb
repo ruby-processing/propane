373676a6ca7b4c83ae545b620da9d24463c3b367
monkstone <mamba2928@yahoo.co.uk>
2016-09-03 15:55:31 +0100

    bump jruby

--- pom.rb
+++ pom.rb
@@ -32,7 +32,7 @@ project 'rp5extras', 'https://github.com/monkstone/propane' do
     'jogl.version' => '2.3.2'
     )
   
-  pom 'org.jruby:jruby:9.1.2.0'
+  pom 'org.jruby:jruby:9.1.4.0'
   jar 'org.processing:core:3.2.1'
   jar 'org.processing:video:3.0.2'
   jar('org.jogamp.jogl:jogl-all:${jogl.version}')
