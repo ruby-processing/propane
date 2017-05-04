d2e53bc4b5f62561f1a3315463b3f5d4249766c6
monkstone <mamba2928@yahoo.co.uk>
2017-05-04 15:24:33 +0100

    apparently working?git add lib!

--- lib/propane/installed_java_library.rb
+++ lib/propane/installed_java_library.rb
@@ -4,7 +4,7 @@ require_relative 'native_folder'
 # The InstalledJavaLibrary class
 class InstalledJavaLibrary
 
-  attr_reader :dir, :path, :name
+  attr_reader :dir, :path, :name, :ppath
 
   def initialize(name)
     @name = name
@@ -26,18 +26,12 @@ class InstalledJavaLibrary
   def native_binaries?
     native_folder = NativeFolder.new
     native = native_folder.name
-    ppath = File.join(dir, native)
+    @ppath = File.join(dir, native)
     File.directory?(ppath) && !Dir.glob(File.join(ppath, native_folder.extension)).empty?
   end
 
   def add_binaries_to_classpath
-    platform_specific_library_paths << java.lang.System.getProperty('java.library.path')
-    new_library_path = platform_specific_library_paths.join(java.io.File.pathSeparator)
-    java.lang.System.setProperty('java.library.path', new_library_path)
-    field = java.lang.Class.for_name('java.lang.ClassLoader').get_declared_field('sys_paths')
-    if field
-      field.accessible = true
-      field.set(java.lang.Class.for_name('java.lang.System').get_class_loader, nil)
-    end
+    native_loader = NativeLoader.new
+    native_loader.add_native_path(ppath)
   end
 end
--- lib/propane/local_java_library.rb
+++ lib/propane/local_java_library.rb
@@ -1,9 +1,10 @@
 require_relative 'native_folder'
+require_relative 'native_loader'
 
 # The LocalJavaLibrary class
 class LocalJavaLibrary
 
-  attr_reader :dir, :path, :name
+  attr_reader :dir, :path, :name, :ppath
 
   def initialize(name)
     @name = name
@@ -24,18 +25,12 @@ class LocalJavaLibrary
   def native_binaries?
     native_folder = NativeFolder.new
     native = native_folder.name
-    ppath = File.join(dir, native)
+    @ppath = File.join(dir, native)
     File.directory?(ppath) && !Dir.glob(File.join(ppath, native_folder.extension)).empty?
   end
 
   def add_binaries_to_classpath
-    platform_specific_library_paths << java.lang.System.getProperty('java.library.path')
-    new_library_path = platform_specific_library_paths.join(java.io.File.pathSeparator)
-    java.lang.System.setProperty('java.library.path', new_library_path)
-    field = java.lang.Class.for_name('java.lang.ClassLoader').get_declared_field('sys_paths')
-    if field
-      field.accessible = true
-      field.set(java.lang.Class.for_name('java.lang.System').get_class_loader, nil)
-    end
+    native_loader = NativeLoader.new
+    native_loader.add_native_path(ppath)
   end
 end
