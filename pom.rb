e525b4dc5e8b9324a28a353a325e82f7e526e462
Martin Prout <mamba2928@yahoo.co.uk>
2020-12-13 16:35:52 +0000

    automatic module

--- pom.rb
+++ pom.rb
@@ -35,48 +35,48 @@ project 'propane', 'https://github.com/monkstone/propane' do
   issue_management 'https://github.com/ruby-processing/propane/issues', 'Github'
 
   source_control(url: 'https://github.com/ruby-processing/propane',
-                 connection: 'scm:git:git://github.com/ruby-processing/propane.git',
-                 developer_connection: 'scm:git:git@github.com/ruby-processing/propane.git')
+    connection: 'scm:git:git://github.com/ruby-processing/propane.git',
+    developer_connection: 'scm:git:git@github.com/ruby-processing/propane.git')
 
-  properties('propane.basedir' => '${project.basedir}',
-             'processing.api' => 'http://processing.github.io/processing-javadocs/core/',
-             'source.directory' => 'src',
-             'polyglot.dump.pom' => 'pom.xml',
-             'project.build.sourceEncoding' => 'utf-8',
-             'jogl.version' => '2.3.2', # for compiling actual included 2.4.0-rc
-             'jruby.api' => 'http://jruby.org/apidocs/')
+    properties('propane.basedir' => '${project.basedir}',
+      'processing.api' => 'http://processing.github.io/processing-javadocs/core/',
+      'source.directory' => 'src',
+      'polyglot.dump.pom' => 'pom.xml',
+      'project.build.sourceEncoding' => 'utf-8',
+      'jogl.version' => '2.3.2', # for compiling actual included 2.4.0-rc
+      'jruby.api' => 'http://jruby.org/apidocs/')
 
-  pom 'org.jruby:jruby:9.2.12.0'
-  jar 'org.processing:video:3.3.7' # only for compiling
-  jar 'org.jogamp.jogl:jogl-all:${jogl.version}'
-  jar 'org.jogamp.gluegen:gluegen-rt-main:${jogl.version}'
+      pom 'org.jruby:jruby:9.2.14.0'
+      jar 'org.processing:video:3.3.7' # only for compiling
+      jar 'org.jogamp.jogl:jogl-all:${jogl.version}'
+      jar 'org.jogamp.gluegen:gluegen-rt-main:${jogl.version}'
 
-  overrides do
-    plugin('org.codehaus.mojo:versions-maven-plugin:2.7',
-           'generateBackupPoms' => 'false')
-    plugin(:compiler, '3.8.1',
-           'release' => '11')
-    plugin(:javadoc, '3.2.0',
-           'detectOfflineLinks' => 'false',
-           'links' => ['${jruby.api}',
-                       '${processing.api}'])
-    plugin(
-      :jar, '3.2.0',
-      'archive' => {
-        'manifestEntries' => { 'Class-Path' => 'gluegen-rt.jar jog-all.jar' }
-      }
-    )
-  end
-  build do
-    resource do
-      directory '${source.directory}/main/java'
-      includes '**/**/*.glsl', '**/*.jnilib'
-      excludes '**/**/*.java'
-    end
-    resource do
-      directory '${source.directory}/main/resources'
-      includes '**/*.png', '*.txt'
-      excludes
-    end
-  end
-end
+      overrides do
+        plugin('org.codehaus.mojo:versions-maven-plugin:2.7',
+          'generateBackupPoms' => 'false')
+          plugin(:compiler, '3.8.1',
+            'release' => '11')
+            plugin(:javadoc, '3.2.0',
+              'detectOfflineLinks' => 'false',
+              'links' => ['${jruby.api}',
+                '${processing.api}'])
+                plugin(:jar, '3.2.0',
+                  'archive' => {
+                    'manifestEntries' => {
+                      'Automatic-Module-Name' => 'processing.core'
+                    }
+                    })                    
+                  end
+                  build do
+                    resource do
+                      directory '${source.directory}/main/java'
+                      includes '**/**/*.glsl', '**/*.jnilib'
+                      excludes '**/**/*.java'
+                    end
+                    resource do
+                      directory '${source.directory}/main/resources'
+                      includes '**/*.png', '*.txt'
+                      excludes
+                    end
+                  end
+                end
